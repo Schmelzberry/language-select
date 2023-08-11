22c6a1f8db8e112a453ab7c0ce74ab1ddde4f76f
Schmelzberry <jelsberry96@gmail.com>
2023-08-11 11:07:04 -0700

    collect form info in one function

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function userInput () {
+const colorInput = document.getElementById("color").value;
+const numberInput = document.getElementById("color").value;
+const ageInput = document.getElementById("color").value;
+const movieInput = document.getElementById("color").value;
+const musicInput = document.getElementById("color").value;
+
+const userInputResults = colorInput + '' + numberInput + '' + ageInput + '' + movieInput + '' + musicInput;
+
+if(userInputResults.length >= 10 && userInputResults.length <= 20)
+
+}
