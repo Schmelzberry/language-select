5ea86595ade31afae3ca8661fe79ab69a3e9e742
Schmelzberry <jelsberry96@gmail.com>
2023-08-11 13:35:20 -0700

    have a branching bug. WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,40 @@
 //Business Logic
 
-function programTest (col,num,bev,mov,mus) {
-const inputSum = mus + mov + bev + num + col;
+function programTest(col, num, bev, mov, mus) {
+  const inputSum = mus + mov + bev + num + col;
 
-if(inputSum.length >= 0 && inputSum.length <= 20) {
-  document.getElementById("ruby").removeAttribute("class");
-} else if (inputSum.length > 20 && inputSum.length < 40) {
-document.getElementById("csharp").removeAttribute("class");
-} else {
-  document.getElementById("rust").removeAttribute("class");
+  if (inputSum.length >= 0 && inputSum.length <= 15) {
+    document.getElementById("ruby").removeAttribute("class");
+  } else if (inputSum.length > 15 && inputSum.length < 35) {
+    document.getElementById("csharp").removeAttribute("class");
+  } else {
+    document.getElementById("rust").removeAttribute("class");
   }
 }
 // UI logic
-window.addEventListener("load", submission); 
+window.addEventListener("load", submission);
 
 function submission() {
-let form = document.getElementById("program");
-form.addEventListener("submit", handleCalculation);
+  let form = document.getElementById("program");
+  form.addEventListener("submit", handleCalculation);
 }
 
 function handleCalculation(event) {
   event.preventDefault();
 
-const colorInput = document.getElementById("color").value;
-const numberInput = document.getElementById("number").value;
-const drinkInput = document.getElementById("drank").value;
-const movieInput = document.getElementById("movie").value;
-const musicInput = document.getElementById("music").value;
+  const colorInput = document.getElementById("color").value;
+  const numberInput = document.getElementById("number").value;
+  const drinkInput = document.getElementById("drank").value;
+  const movieInput = document.getElementById("movie").value;
+  const musicInput = document.getElementById("music").value;
 
-const userInputResults = programTest(colorInput,numberInput,drinkInput,movieInput, musicInput);
+  let userInputResults = parseInt(programTest(colorInput, numberInput, drinkInput, movieInput, musicInput));
 
-document.getElementById("output-rust").innerText = "the programming language best suited to you is Rust based on your response of: " + userInputResults; + " !"
-
+  if (userInputResults >= 0 && userInputResults <= 15) {
+    document.querySelector("h2#output-ruby").innerText = "The programming language best suited to you is Rust based on your response of: " + userInputResults; + " !";
+  } else if (userInputResults > 15 && userInputResults < 35) {
+    document.querySelector("h2#output-csharp").innerText = "Ayyyyyy, the language suited to you is Rust based on your response of: " + userInputResults; + " !";
+  } else {
+    document.getElementById("output-rust").innerText = "The language suited to you is Rust based on your response of: " + userInputResults; + " !";
+  }
 }
-
-
-
-// function formSubmission() {
-//   let form = document.querySelector("form");
-//   form.onsubmit = function(event) {
-//     event.preventDefault();
-//     userInput();
-//   }
-
-
-
-
-//   window.onload = function () {
-//   formSubmission();
-//     }
-  
-//   }
