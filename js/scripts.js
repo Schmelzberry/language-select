e524d8d6c87bdbb32f587b9974630263e7cc607f
Schmelzberry <jelsberry96@gmail.com>
2023-08-11 13:13:45 -0700

    feature a second branch in UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 //Business Logic
 
-function programTest (col,num,ag,mov,mus) {
-const inputSum = mus + mov + ag + num + col;
+function programTest (col,num,bev,mov,mus) {
+const inputSum = mus + mov + bev + num + col;
 
 if(inputSum.length >= 0 && inputSum.length <= 20) {
   document.getElementById("ruby").removeAttribute("class");
@@ -9,27 +9,28 @@ if(inputSum.length >= 0 && inputSum.length <= 20) {
 document.getElementById("csharp").removeAttribute("class");
 } else {
   document.getElementById("rust").removeAttribute("class");
-  
   }
 }
 // UI logic
 window.addEventListener("load", submission); 
+
 function submission() {
 let form = document.getElementById("program");
 form.addEventListener("submit", handleCalculation);
 }
+
 function handleCalculation(event) {
   event.preventDefault();
 
 const colorInput = document.getElementById("color").value;
 const numberInput = document.getElementById("number").value;
-const ageInput = document.getElementById("age").value;
+const drinkInput = document.getElementById("drank").value;
 const movieInput = document.getElementById("movie").value;
 const musicInput = document.getElementById("music").value;
 
-const userInputResults = programTest(colorInput,numberInput,ageInput,movieInput, musicInput);
+const userInputResults = programTest(colorInput,numberInput,drinkInput,movieInput, musicInput);
 
-
+document.getElementById("output-rust").innerText = "the programming language best suited to you is Rust based on your response of: " + userInputResults; + " !"
 
 }
 
