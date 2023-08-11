6f57e4db80b1678bb9608f5a35752c48586e9668
Schmelzberry <jelsberry96@gmail.com>
2023-08-11 11:21:35 -0700

    prevent form from clearing upon submission

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//Business Logic
+
 function userInput () {
 const colorInput = document.getElementById("color").value;
 const numberInput = document.getElementById("color").value;
@@ -7,6 +9,22 @@ const musicInput = document.getElementById("color").value;
 
 const userInputResults = colorInput + '' + numberInput + '' + ageInput + '' + movieInput + '' + musicInput;
 
-if(userInputResults.length >= 10 && userInputResults.length <= 20)
+if(userInputResults.length >= 10 && userInputResults.length <= 20) {
+  document.getElementById("results").innerText = "Ruby is the language for you!";
+  document.getElementById("result-info").innerText = "A dynamic, open source programming language with a focus on simplicity and productivity. It has an elegant syntax that is natural to read and easy to write.";
+}
 
+}
+
+// UI logic
+
+function formSubmission() {
+  let form = document.querySelector("form");
+  form.onsubmit = function(event) {
+    event.preventDefault();
+  }
+}
+
+window.onload = function () {
+  formSubmission();
 }
