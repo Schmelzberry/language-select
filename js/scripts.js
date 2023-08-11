ec833efbba409771a30f158c25baf3e74774aa31
Schmelzberry <jelsberry96@gmail.com>
2023-08-11 11:33:50 -0700

    introduce branching logic, form behavior WIP

--- js/scripts.js
+++ js/scripts.js
@@ -9,10 +9,17 @@ const musicInput = document.getElementById("color").value;
 
 const userInputResults = colorInput + '' + numberInput + '' + ageInput + '' + movieInput + '' + musicInput;
 
-if(userInputResults.length >= 10 && userInputResults.length <= 20) {
+if(userInputResults.length >= 0 && userInputResults.length <= 20) {
   document.getElementById("results").innerText = "Ruby is the language for you!";
   document.getElementById("result-info").innerText = "A dynamic, open source programming language with a focus on simplicity and productivity. It has an elegant syntax that is natural to read and easy to write.";
 }
+else if (userInputResults.length > 20 && userInputResults.length <= 40) {
+document.getElementById("results").innerText = "C# is the language for you!";
+document.getElementById("result-info").innerText = "C# (C-Sharp) is a programming language developed by Microsoft that runs on the .NET Framework.";
+
+} else {
+  document.getElementById("results").innerText = "Java"
+  document.getElementById("results-info").innerText = "Java does some things"
 
 }
 
@@ -23,8 +30,9 @@ function formSubmission() {
   form.onsubmit = function(event) {
     event.preventDefault();
   }
+userInput();
 }
 
 window.onload = function () {
   formSubmission();
-}
+}}
