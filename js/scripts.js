93047439565ee4207db9f59f59b2b0dea7860bb9
Schmelzberry <jelsberry96@gmail.com>
2023-08-11 12:55:29 -0700

    show 3 different results to user depending on string length

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,52 @@
 //Business Logic
 
-function userInput () {
-const colorInput = document.getElementById("color").value;
-const numberInput = document.getElementById("color").value;
-const ageInput = document.getElementById("color").value;
-const movieInput = document.getElementById("color").value;
-const musicInput = document.getElementById("color").value;
+function programTest (col,num,ag,mov,mus) {
+const inputSum = mus + mov + ag + num + col;
 
-const userInputResults = colorInput + '' + numberInput + '' + ageInput + '' + movieInput + '' + musicInput;
-
-if(userInputResults.length >= 0 && userInputResults.length <= 20) {
-  document.getElementById("results").innerText = "Ruby is the language for you!";
-  document.getElementById("result-info").innerText = "A dynamic, open source programming language with a focus on simplicity and productivity. It has an elegant syntax that is natural to read and easy to write.";
-}
-else if (userInputResults.length > 20 && userInputResults.length <= 40) {
-document.getElementById("results").innerText = "C# is the language for you!";
-document.getElementById("result-info").innerText = "C# (C-Sharp) is a programming language developed by Microsoft that runs on the .NET Framework.";
-
+if(inputSum.length >= 0 && inputSum.length <= 20) {
+  document.getElementById("ruby").removeAttribute("class");
+} else if (inputSum.length > 20 && inputSum.length < 40) {
+document.getElementById("csharp").removeAttribute("class");
 } else {
-  document.getElementById("results").innerText = "Java"
-  document.getElementById("results-info").innerText = "Java does some things"
-
-}
-
-// UI logic
-
-function formSubmission() {
-  let form = document.querySelector("form");
-  form.onsubmit = function(event) {
-    event.preventDefault();
+  document.getElementById("rust").removeAttribute("class");
+  
   }
-userInput();
+}
+// UI logic
+window.addEventListener("load", submission); 
+function submission() {
+let form = document.getElementById("program");
+form.addEventListener("submit", handleCalculation);
+}
+function handleCalculation(event) {
+  event.preventDefault();
+
+const colorInput = document.getElementById("color").value;
+const numberInput = document.getElementById("number").value;
+const ageInput = document.getElementById("age").value;
+const movieInput = document.getElementById("movie").value;
+const musicInput = document.getElementById("music").value;
+
+const userInputResults = programTest(colorInput,numberInput,ageInput,movieInput, musicInput);
+
+
+
 }
 
-window.onload = function () {
-  formSubmission();
-}}
+
+
+// function formSubmission() {
+//   let form = document.querySelector("form");
+//   form.onsubmit = function(event) {
+//     event.preventDefault();
+//     userInput();
+//   }
+
+
+
+
+//   window.onload = function () {
+//   formSubmission();
+//     }
+  
+//   }
